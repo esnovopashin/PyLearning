c8891dba693fba49f804d218ca10aa257c7a8777
esnovopashin <nes2010@yandex.ru>
2025-01-08 22:12:40 +0500

    ДЗ: простой калькулятор

--- main.py
+++ main.py
@@ -1,1 +1,62 @@
-print("Bears love honey and I'm a Pooh bear")
+#Сложение
+try:
+    num1 = float(input("Введите первое число: "))
+    num2 = float(input("Введите второе число: "))
+
+    result: float = num1 + num2
+
+except ValueError:(
+    print("Ошибка: введено не число!"))
+
+# except ZeroDivisionError:(
+#     print("Ошибка: Деление на ноль!"))
+
+finally:
+    print(f"Результат: {result}")
+
+#Вычитание
+try:
+    num1 = float(input("Введите первое число: "))
+    num2 = float(input("Введите второе число: "))
+
+    result: float = num1 - num2
+
+except ValueError:(
+    print("Ошибка: введено не число!"))
+
+# except ZeroDivisionError:(
+#     print("Ошибка: Деление на ноль!"))
+
+finally:
+    print(f"Результат: {result}")
+
+#Умножение
+try:
+    num1 = float(input("Введите первое число: "))
+    num2 = float(input("Введите второе число: "))
+
+    result: float = num1 * num2
+
+except ValueError:(
+    print("Ошибка: введено не число!"))
+
+# except ZeroDivisionError:(
+#     print("Ошибка: Деление на ноль!"))
+
+finally:
+    print(f"Результат: {result}")
+
+#Деление
+try:
+    num1 = float(input("Введите первое число: "))
+    num2 = float(input("Введите второе число: "))
+
+    result: float = num1 / num2
+
+except ValueError:(
+    print("Ошибка: введено не число!"))
+
+ # except ZeroDivisionError:(
+ #     print("Ошибка: Деление на ноль!"))
+
+finally: print(f"Результат: {result}")
