8a7442d4d3215d3bb6df8e884aa35e45b5a77b9f
esnovopashin <nes2010@yandex.ru>
2025-01-12 14:45:44 +0500

    ДЗ: Работа со строками 1

--- escape_sequences.py
+++ escape_sequences.py
@@ -3,9 +3,14 @@ string_2 = "одинарные \'кавычки\' внутри текста"
 string_3 = "содержит обратный слэш \\ внутри текста"
 string_4 = "текст разделяется на \nдве строки"
 string_5 = "Column 1 \tColumn 2 \tColumn 3\nColumn 1 \tColumn 2 \tColumn 3"
+string_6 = """Демонстрация, как тройные "кавычки" 'помогают' сохранить \форматирование строки 
+без необходимости экранирования символов
+Column 1  Column 2  Column 3
+Column 1  Column 2  Column 3"""
 
 print(string_1, '\n')
 print(string_2, '\n')
 print(string_3, '\n')
 print(string_4, '\n')
-print(string_5)
+print(string_5, '\n')
+print(string_6)
---
+++ strings_in_python_1.py
@@ -0,0 +1,8 @@
+my_name = '\"Eugeny\"'
+my_hobby = "\'Playing guitar\'"
+my_favorite_film = """My favorite film is
+'Star Trek: Next Generation'"""
+
+print(my_name)
+print(my_hobby)
+print(my_favorite_film)
