f740756a4c8cacfb600f59f73c977a1a1a237de6
esnovopashin <nes2010@yandex.ru>
2025-01-12 13:47:50 +0500

    Добавил в вывод заголовки арифметическим действиям и отступы

---
+++ escape_sequences.py
@@ -0,0 +1,10 @@
+string_1 = "двойные \"кавычки\" внутри текста"
+string_2 = "одинарные \'кавычки\' внутри текста"
+string_3 = "содержит обратный слэш \\ внутри текста"
+string_4 = "текст разделяется на \nдве строки"
+string_5 = "Column 1 \tColumn 2 \tColumn 3\nColumn 1 \tColumn 2 \tColumn 3"
+print(string_1, '\n')
+print(string_2, '\n')
+print(string_3, '\n')
+print(string_4, '\n')
+print(string_5)
