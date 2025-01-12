16a951ef5d72a0e9476613e26bb86f1e222ea2e8
esnovopashin <nes2010@yandex.ru>
2025-01-12 13:50:30 +0500

    ДЗ: Экранирующие последовательности

--- escape_sequences.py
+++ escape_sequences.py
@@ -3,6 +3,7 @@ string_2 = "одинарные \'кавычки\' внутри текста"
 string_3 = "содержит обратный слэш \\ внутри текста"
 string_4 = "текст разделяется на \nдве строки"
 string_5 = "Column 1 \tColumn 2 \tColumn 3\nColumn 1 \tColumn 2 \tColumn 3"
+
 print(string_1, '\n')
 print(string_2, '\n')
 print(string_3, '\n')
