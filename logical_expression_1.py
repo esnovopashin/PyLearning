9554ac13398b1088c434805153d06e4c926aa493
esnovopashin <nes2010@yandex.ru>
2025-11-03 20:55:30 +0500

    ДЗ: Логические выражения и условия

---
+++ logical_expression_1.py
@@ -0,0 +1,8 @@
+age = 18
+is_citizenship = True
+is_disqualified = False
+
+if age >= 18 and is_citizenship and not is_disqualified:
+    print("Может голосовать")
+else:
+    print("Не может голосовать")
