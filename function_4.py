9244f211b2656a67747cd74367379a876adf128d
esnovopashin <nes2010@yandex.ru>
2025-11-03 17:28:14 +0500

    4 задания из бока Функции_на_правктике

---
+++ function_4.py
@@ -0,0 +1,29 @@
+#1
+def max_number(a, b):
+    if a > b:
+        return a
+    elif b > a:
+        return b
+    else:
+        return a  # Если числа равны
+    
+#2
+def empty_function():
+    pass
+
+#3
+def even_numbers(n):
+    for i in range(0, n+1, 2):
+        yield i
+
+#4 автотест для функции max_number(a, b)
+def test_max_number():
+
+    assert max_number(1, 3) == 5, "Error: 3 > 1"
+    assert max_number(-3, -1) == -1, "Error: -1 > -3"
+    assert max_number(0, 0) == 0, "Error: если a=b, выводим a"
+    assert max_number(10, 5) == 10, "Error: 10 > 5"
+    
+    print("Passed")
+    
+test_max_number()
