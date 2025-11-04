ed53135d7821d4f18ca3f9880d4120ce6557ddc2
esnovopashin <nes2010@yandex.ru>
2025-11-04 15:47:21 +0500

    добавил проверку вводимых значений, исключение на невалидный ввод

--- number_to_word.py
+++ number_to_word.py
@@ -1,4 +1,3 @@
-num = int(input("Введите число от 1 до 5: "))
 def num_to_word(num):
     if num == 1:
         print("One")
@@ -8,10 +7,23 @@ def num_to_word(num):
         print("Three")
     elif num == 4:
         print("Four")
-    elif num ==5:
+    elif num == 5:
         print("Five")
     else:
-        num = int(input("Введите число от 1 до 5: "))
+        print("Число должно быть от 1 до 5")
+        num = number_input()
         num_to_word(num)
 
+def number_input():
+    while True:
+        try:
+            num = int(input("Введите число от 1 до 5: ").strip())
+            if 1 <= num <= 5:
+                return num
+            else:
+                print("Ошибка! Введите число в диапазоне от 1 до 5")
+        except ValueError:
+            print("Ошибка! Введите только цифры")
+
+num = number_input()
 num_to_word(num)
