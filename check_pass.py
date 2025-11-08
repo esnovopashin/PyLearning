e35ec856b17112e892fe820e3ec148dd7fd306ea
esnovopashin <nes2010@yandex.ru>
2025-11-08 18:48:17 +0500

    ДЗ: Проверка пароля. Добавил пользовательский ввод начального пароля. Количество букв задается размером начального пароля

--- check_pass.py
+++ check_pass.py
@@ -1,21 +1,25 @@
-def user_input():
-    user_pswrd = str(
-        input("Введите пароль: ").lower()
-        )
-    if len(pswrd) < len(user_pswrd):
-        user_pswrd = str(
-            input(f"Ошибка! Количество букв больше {len(pswrd)} Введите пароль: ").lower()
-        )
-    elif len(pswrd) > len(user_pswrd):
-        user_pswrd = str(
-            input(f"Ошибка! Количество букв меньше {len(pswrd)}. Введите пароль: ").lower()
-            )
+def pswrd_input():
+    pswrd = input("Введите начальный пароль: ").lower().strip()
+        
+    return pswrd
+
+
+def user_input(pswrd):
+    user_pswrd = input("Введите пароль: ").lower().strip()
+    
+    while len(user_pswrd) != len(pswrd):
+        if len(user_pswrd) < len(pswrd):
+            user_pswrd = input(f"Ошибка! Количество букв меньше {len(pswrd)}. Введите пароль: ").lower()
+        elif len(user_pswrd) > len(pswrd):
+            user_pswrd = input(f"Ошибка! Количество букв больше {len(pswrd)}. Введите пароль: ").lower()
+        
     return user_pswrd
+
 
 def check_pswrd():
     while True:
         try:
-            user_pswrd = user_input()
+            user_pswrd = user_input(pswrd)
             if pswrd == user_pswrd:
                 print("Молодец, у тебя получилось!")
                 break
@@ -24,15 +28,12 @@ def check_pswrd():
     
         except ValueError:
             print("Что-то пошло не так")
-   
-pswrd = "juni0r"
-user_pswrd = " "
 
+pswrd = pswrd_input()
 print("=" * 50)
 print("Введите пароль. Допускаются только прописные латинские буквы и цифры \nЕсли пароль не подобран, можно попробовать снова \nУдачи!")
 print(f"Количество букв {len(pswrd)}")
 print("=" * 50)
-
 
 if __name__ == "__main__":
     check_pswrd()
