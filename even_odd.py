3652099d9d20f3347d8ff71f2a6d90d2fbbcef12
esnovopashin <nes2010@yandex.ru>
2025-11-17 01:37:50 +0500

    задание 3

--- even_odd.py
+++ even_odd.py
@@ -1,32 +1,48 @@
-#1. Напишите программу, которая вычисляет сумму всех четных чисел от 1 до 100.
-#2. Создайте список, содержащий квадраты всех нечетных чисел от 1 до 10, используя **генератор списка**.
-#3. Напишите программу, которая просит пользователя ввести число и продолжает запрашивать числа, пока пользователь не введет отрицательное число. Затем программа должна вывести количество введенных чисел
-
-numbers = []
 even_num = []
 odd_num = []
 sq_odd_numbers = []
-num = 0
 numbers = [num for num in range(0,101)]
-print(f"Длина массива: ", len(numbers))
+#print(f"Длина массива: ", len(numbers))
 
-#1
+def verify_user_input():
+    count = 0
+    while True:
+        try:
+            user_input = int(
+                input("Введите число : ").strip()
+            )
+
+            if user_input < 0:
+                break
+            count += 1
+        
+        except ValueError:
+            print("Ошибка! Введите число")
+    
+    print("-" * 25)        
+    print(f"Введено чисел: ", count)
+    
+    return count
+
+print("=" * 25, "Задание 1 ", "=" * 25)
+
 for num in numbers:
     if num % 2 == 0:
         even_num.append(num)
-print(f"четные числа: ", even_num)
 
-#sum_even_num = sum(even_num)
+print(f"четные числа: ", even_num)
 print(f"Сумма четных чисел: ", sum(even_num))
 
-#2
+print("=" * 25, "Задание 2 ", "=" * 25)
+
 for num in numbers:
     if num % 2 != 0:
         odd_num.append(num)
-print(f"Нечетные числа: {odd_num}")
+print(f"Нечетные числа: ", odd_num)
 
 sq_odd_numbers = [(lambda num: num*num)(num) for num in odd_num]
-print(sq_odd_numbers)
+print(f"Квадраты нечетных чисел: ", sq_odd_numbers)
 
-
-#3
+if __name__ == "__main__":
+    print("=" * 25, "Задание 3 ", "=" * 25)
+    counter_user_input = verify_user_input()
