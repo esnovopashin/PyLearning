c8b6425ae2b0c0e984d64810045d02491c69859c
esnovopashin <nes2010@yandex.ru>
2025-11-22 21:15:31 +0500

    Сумма четных выводится без использования промежуточного массива

--- even_odd.py
+++ even_odd.py
@@ -19,25 +19,19 @@ def verify_user_input():
     return count
 
 
-numbers = [num for num in range(0,101)]
-
 # Задача 1
 print("=" * 25, "Задание 1 ", "=" * 25)
-even_num = []
+numbers = [num for num in range(0,101)]
+summ_even_num = 0
+
 for num in numbers:
     if num % 2 == 0:
-        even_num.append(num)
+        summ_even_num += num
 
-print(f"четные числа: ", even_num)
-print(f"Сумма четных чисел: ", sum(even_num))
+print(f"Сумма четных чисел: ", summ_even_num)
 
 # Задача 2
 print("=" * 25, "Задание 2 ", "=" * 25)
-
-# for num in numbers:
-#     if num % 2 != 0:
-#         odd_num.append(num)
-# print(f"Нечетные числа: ", odd_num)
 
 odd_num = [num for num in range(1, 11, 2)]
 print(f"Квадраты нечетных чисел: ", [num ** 2 for num in odd_num])
