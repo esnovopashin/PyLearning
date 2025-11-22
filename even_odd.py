cb5f6ae623cb507a996563647aac300620a15818
esnovopashin <nes2010@yandex.ru>
2025-11-22 19:03:07 +0500

    revert latest changes

--- even_odd.py
+++ even_odd.py
@@ -1,7 +1,8 @@
-
-odd_num = [num for num in range(1, 10, 2)]
+even_num = []
+odd_num = []
+sq_odd_numbers = []
 numbers = [num for num in range(0,101)]
-
+#print(f"Длина массива: ", len(numbers))
 
 def verify_user_input():
     count = 0
@@ -24,15 +25,24 @@ def verify_user_input():
     return count
 
 print("=" * 25, "Задание 1 ", "=" * 25)
-even_num = [num for num in numbers if num % 2 == 0]
+
+for num in numbers:
+    if num % 2 == 0:
+        even_num.append(num)
+
 print(f"четные числа: ", even_num)
 print(f"Сумма четных чисел: ", sum(even_num))
 
 print("=" * 25, "Задание 2 ", "=" * 25)
-sq_odd_numbers = [num ** 2 for num in odd_num]
+
+for num in numbers:
+    if num % 2 != 0:
+        odd_num.append(num)
+print(f"Нечетные числа: ", odd_num)
+
+sq_odd_numbers = [(lambda num: num*num)(num) for num in odd_num]
 print(f"Квадраты нечетных чисел: ", sq_odd_numbers)
 
 if __name__ == "__main__":
     print("=" * 25, "Задание 3 ", "=" * 25)
-    print(f"Подсчитаем количество введенных чисел\nВвод принимается, пока не будет введено отрицательное число")
     counter_user_input = verify_user_input()
